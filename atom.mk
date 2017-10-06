7178f3b561c78d7ab58a29f7f5c9558656d5523a
Yves-Marie Morgan <yves-marie.morgan@parrot.com>
2017-10-06 14:34:44 +0200

    [DEV] add option for minimal AVC/HEVC decoding with HW acceleration
    - option FFMPEG_MINIMAL_AVC_HEVC_DECODING builds the minimum required
    for H.264/AVC and H.265/HEVC decoding
    - option FFMPEG_ENABLE_CUVID enables decoding with NVDec (cuvid)
    hardware acceleration
    WARNING: non-free software is enabled in this configuration,
    the software must not be distributed with cuvid enabled
    - option FFMPEG_ENABLE_VDPAU enables decoding with VDPAU
    hardware acceleration

--- atom.mk
+++ atom.mk
@@ -11,6 +11,9 @@ LOCAL_CATEGORY_PATH := multimedia/ffmpeg
 
 LOCAL_DESCRIPTION := cross-platform tools and libraries to convert, manipulate and stream a wide range of multimedia formats and protocols
 
+LOCAL_CONFIG_FILES := aconfig.in
+$(call load-config)
+
 
 # Main compilation options
 LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
@@ -25,6 +28,46 @@ ifeq ("$(TARGET_ARCH)","x64")
 else
   LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --arch="$(TARGET_ARCH)"
 endif
+
+ifdef CONFIG_FFMPEG_MINIMAL_AVC_HEVC_DECODING
+
+# Components options
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--disable-all \
+	--enable-avcodec \
+	--enable-decoder=h264 \
+	--enable-decoder=hevc
+
+ifdef CONFIG_FFMPEG_ENABLE_CUVID
+
+# WARNING: non-free software is enabled in this configuration,
+# the software must not be distributed with cuvid enabled.
+
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-decoder=h264_cuvid \
+	--enable-decoder=hevc_cuvid \
+	--enable-nvenc \
+	--enable-cuda \
+	--enable-cuvid \
+	--enable-libnpp \
+	--enable-nonfree \
+	--extra-cflags=-I/usr/local/cuda/include \
+	--extra-ldflags=-L/usr/local/cuda/lib64
+
+endif
+
+ifdef CONFIG_FFMPEG_ENABLE_VDPAU
+
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-decoder=h264_vdpau \
+	--enable-vdpau
+
+endif
+
+# Export libraries
+LOCAL_EXPORT_LDLIBS = -lavcodec -lavutil
+
+else
 
 # Components options
 LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
@@ -46,6 +89,8 @@ LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
 	--disable-gpl \
 	--disable-version3 \
 	--disable-nonfree
+
+endif
 
 LOCAL_LIBRARIES := zlib
 
