9b36d3fec6522a44f1e4a55b60c5ef5aca4678b0
Nicolas Brûlez <nicolas.brulez@parrot.com>
2021-01-07 20:23:25 +0100

    [BUILD] Add atom.mk, police license files and upstream sync script
    Note: this is a squash of commits on the 3.3 version branch

---
+++ atom.mk
@@ -0,0 +1,124 @@
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ffmpeg-libav
+LOCAL_CATEGORY_PATH := multimedia/ffmpeg
+LOCAL_DESCRIPTION := Cross-platform tools and libraries to convert, manipulate and stream a wide range of multimedia formats and protocols
+
+LOCAL_CONFIG_FILES := aconfig.in
+$(call load-config)
+
+ifeq ("$(TARGET_ARCH)","x64")
+  LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --arch="x86_64"
+else
+  LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --arch="$(TARGET_ARCH)"
+endif
+
+# Main compilation options
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-shared \
+	--enable-cross-compile \
+	--enable-optimizations \
+	--cross-prefix="$(TARGET_CROSS)"
+
+ifeq ("$(TARGET_OS)","windows")
+  ifeq ("$(TARGET_ARCH)","x86")
+    LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --target-os="mingw32"
+  else ifeq ("$(TARGET_ARCH)","x64")
+    LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --target-os="mingw64"
+  endif
+else
+  LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --target-os="$(TARGET_OS)"
+endif
+
+# Components options
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--disable-orc \
+	--disable-avconv \
+	--disable-avplay \
+	--disable-avprobe \
+	--disable-avserver \
+	--disable-avdevice \
+	--disable-avresample \
+	--disable-filters \
+	--disable-bzlib \
+	--disable-stripping
+
+# Licensing options
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--disable-gpl \
+	--disable-version3 \
+	--disable-nonfree
+
+# User selected components
+#
+# By default all decoders/encoders/parsers/muxers/demuxers are disabled to
+# reduce the compilation time.
+# When a user needs a specific component a new configuration should be added
+# to 'aconfig.in' and an entry should be added in this section
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --disable-everything
+
+ifdef CONFIG_FFMPEG_HEVC_DECODING
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec \
+	--enable-decoder=hevc
+endif
+
+ifdef CONFIG_FFMPEG_AVC_DECODING
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec \
+	--enable-decoder=h264
+endif
+
+ifdef CONFIG_FFMPEG_AAC_ENCODING
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec \
+	--enable-encoder=aac
+endif
+
+ifdef CONFIG_FFMPEG_MOV_FORMAT
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-demuxer=mov \
+	--enable-muxer=mov
+endif
+
+ifdef CONFIG_FFMPEG_PROGRAMS
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-protocol=file \
+	--enable-demuxer=h264 \
+	--enable-muxer=h264 \
+	--enable-parser=h264 \
+	--enable-muxer=mp4
+else
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--disable-programs
+endif
+
+ifdef CONFIG_FFMPEG_ENABLE_VDPAU
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-vdpau \
+	--enable-decoder=h264_vdpau
+endif
+
+# License check (shall be the last rule)
+ifneq (,$(filter --enable-nonfree --enable-version3 --enable-nonfree, \
+	$(LOCAL_AUTOTOOLS_CONFIGURE_ARGS)))
+$(warning some options: "$(filter --enable-nonfree --enable-version3 \
+	--enable-nonfree, $(LOCAL_AUTOTOOLS_CONFIGURE_ARGS))" \
+	are not compatible with a release)
+endif
+
+# Export libraries
+LOCAL_EXPORT_LDLIBS = \
+	-lavcodec \
+	-lavutil \
+	-lavformat \
+	-lavfilter \
+	-lswresample \
+	-lswscale
+
+LOCAL_LIBRARIES := zlib
+
+include $(BUILD_AUTOTOOLS)
