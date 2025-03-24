004b4713bdb6722e96de5f40f22c152f0d4f1ad0
Aurélien Barre <aurelien.barre@parrot.com>
2025-03-24 18:33:25 +0100

    [DEV] Ground SDK Tools 8.2 release

---
+++ atom.mk
@@ -0,0 +1,217 @@
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ("$(TARGET_OS_FLAVOUR)","native")
+  # Optional prebuilt package: OpenH264 prebuilt binaries
+  $(call register-prebuilt-pkg-config-module,libopenh264,openh264)
+endif
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := ffmpeg-libav
+LOCAL_CATEGORY_PATH := multimedia/ffmpeg
+LOCAL_DESCRIPTION := Cross-platform tools and libraries to convert, manipulate and stream a wide range of multimedia formats and protocols
+
+LOCAL_CONFIG_FILES := aconfig.in
+$(call load-config)
+
+ifeq ("$(TARGET_ARCH)","x64")
+  LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --arch="x86_64"
+else
+  LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --arch="$(TARGET_ARCH)"
+endif
+
+# Main compilation options
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-shared \
+	--enable-cross-compile \
+	--enable-optimizations \
+	--cross-prefix="$(TARGET_CROSS)" \
+	--cc=$(TARGET_CC) \
+	--cxx=$(TARGET_CXX)
+
+ifeq ("$(TARGET_OS)","windows")
+  ifeq ("$(TARGET_ARCH)","x86")
+    LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --target-os="mingw32"
+  else ifeq ("$(TARGET_ARCH)","x64")
+    LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --target-os="mingw64"
+  endif
+else
+  LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --target-os="$(TARGET_OS)"
+endif
+
+# on x86_*, nasm is expected
+ffmpeg_use_nasm := $(if $(filter $(TARGET_ARCH),x86 x64),1,0)
+ifeq ("$(ffmpeg_use_nasm)","1")
+  LOCAL_DEPENDS_HOST_MODULES := host.nasm
+else
+  LOCAL_AUTOTOOLS_CONFIGURE_ARGS += --disable-x86asm
+endif
+
+# Optional NVCODEC HW encoding/decoding support: use the nvidia headers
+ffmpeg_use_nvcodec := $(call is-module-in-build-config,ffnvcodec)
+ifneq ("$(ffmpeg_use_nvcodec)","")
+LOCAL_DEPENDS_MODULES += \
+	ffnvcodec
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-nvdec \
+	--enable-nvenc
+endif
+
+# Optional OpenH264 encoding support
+ffmpeg_use_openh264 := $(call is-module-in-build-config,libopenh264)
+ifneq ("$(ffmpeg_use_openh264)","")
+LOCAL_DEPENDS_MODULES += \
+	libopenh264
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-libopenh264
+endif
+
+# Components options
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--disable-orc \
+	--disable-avconv \
+	--disable-avplay \
+	--disable-avprobe \
+	--disable-avserver \
+	--disable-avdevice \
+	--disable-avresample \
+	--disable-filters \
+	--disable-bzlib \
+	--disable-stripping
+
+# Licensing options
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--disable-gpl \
+	--disable-version3 \
+	--disable-nonfree
+
+# User selected components
+#
+# By default all decoders/encoders/parsers/muxers/demuxers are disabled to
+# reduce the compilation time.
+# When a user needs a specific component a new configuration should be added
+# to 'aconfig.in' and an entry should be added in this section
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--disable-all \
+	--disable-everything
+
+ifdef CONFIG_FFMPEG_HEVC_DECODING
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec \
+	--enable-decoder=hevc
+ifneq ("$(ffmpeg_use_nvcodec)","")
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-hwaccel=hevc_nvdec
+endif
+LOCAL_EXPORT_LDLIBS += \
+	-lavcodec \
+	-lavutil
+endif
+
+ifdef CONFIG_FFMPEG_AVC_DECODING
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec \
+	--enable-decoder=h264
+ifneq ("$(ffmpeg_use_nvcodec)","")
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-hwaccel=h264_nvdec
+endif
+LOCAL_EXPORT_LDLIBS += \
+	-lavcodec \
+	-lavutil
+endif
+
+ifdef CONFIG_FFMPEG_HEVC_ENCODING
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec
+ifneq ("$(ffmpeg_use_nvcodec)","")
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-encoder=hevc_nvenc
+endif
+LOCAL_EXPORT_LDLIBS += \
+	-lavcodec \
+	-lavutil
+endif
+
+ifdef CONFIG_FFMPEG_AVC_ENCODING
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec
+ifneq ("$(ffmpeg_use_nvcodec)","")
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-encoder=h264_nvenc
+endif
+ifneq ("$(ffmpeg_use_openh264)","")
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-encoder=libopenh264
+endif
+LOCAL_EXPORT_LDLIBS += \
+	-lavcodec \
+	-lavutil
+endif
+
+ifdef CONFIG_FFMPEG_AAC_ENCODING
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec \
+	--enable-encoder=aac
+LOCAL_EXPORT_LDLIBS += \
+	-lavcodec \
+	-lavutil
+endif
+
+ifdef CONFIG_FFMPEG_MOV_FORMAT
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avformat \
+	--enable-demuxer=mov \
+	--enable-muxer=mov
+endif
+
+ifdef CONFIG_FFMPEG_PROGRAMS
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-avcodec \
+	--enable-avfilter \
+	--enable-avformat \
+	--enable-swresample \
+	--enable-swscale \
+	--enable-ffmpeg \
+	--enable-ffplay \
+	--enable-ffprobe \
+	--enable-protocol=file \
+	--enable-demuxer=h264 \
+	--enable-muxer=h264 \
+	--enable-parser=h264 \
+	--enable-demuxer=mov \
+	--enable-muxer=mp4
+LOCAL_EXPORT_LDLIBS += \
+	-lavcodec \
+	-lavutil \
+	-lavformat \
+	-lavfilter \
+	-lswresample \
+	-lswscale
+else
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--disable-programs
+endif
+
+ifdef CONFIG_FFMPEG_ENABLE_VDPAU
+LOCAL_AUTOTOOLS_CONFIGURE_ARGS += \
+	--enable-vdpau \
+	--enable-decoder=h264_vdpau
+endif
+
+# License check (shall be the last rule)
+ifneq (,$(filter --enable-nonfree --enable-version3 --enable-nonfree, \
+	$(LOCAL_AUTOTOOLS_CONFIGURE_ARGS)))
+$(warning some options: "$(filter --enable-nonfree --enable-version3 \
+	--enable-nonfree, $(LOCAL_AUTOTOOLS_CONFIGURE_ARGS))" \
+	are not compatible with a release)
+endif
+
+LOCAL_LIBRARIES := zlib
+
+define LOCAL_AUTOTOOLS_CMD_POST_INSTALL
+	@rm -rf $(TARGET_OUT_STAGING)/usr/share/ffmpeg
+endef
+
+include $(BUILD_AUTOTOOLS)
